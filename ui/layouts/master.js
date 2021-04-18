6b19e8a85b274cd4f2bf5a3cbed631ea9e742aa9
Elwin Stephan <elwin.stephan@gmail.com>
2021-04-18 19:22:15 +0200

    some commits

--- ui/layouts/master.js
+++ ui/layouts/master.js
@@ -14,32 +14,20 @@
   }
   ```
 */
-import { Fragment, useState } from 'react'
-import { Dialog, Menu, Transition } from '@headlessui/react'
-import {
-    BellIcon,
-    CalendarIcon,
-    ChartBarIcon,
-    FolderIcon,
-    HomeIcon,
-    InboxIcon,
-    MenuAlt2Icon,
-    UsersIcon,
-    XIcon,
-    MapIcon,
-} from '@heroicons/react/outline'
-import { useRouter } from 'next/router'
-
+import {Fragment, useState} from 'react'
+import {Dialog, Menu, Transition} from '@headlessui/react'
+import {BellIcon, ChartBarIcon, HomeIcon, MapIcon, MenuAlt2Icon, XIcon} from '@heroicons/react/outline'
+import {useRouter} from 'next/router'
 
 const navigation = [
-    { name: 'Dashboard', href: '/dashboard', icon: HomeIcon, current: true },
-    { name: 'Map', href: '/map', icon: MapIcon, current: false },
-    { name: 'Statistics', href: '/statistics', icon: ChartBarIcon, current: false },
+    {name: 'Dashboard', href: '/dashboard', icon: HomeIcon, current: true},
+    {name: 'Map', href: '/map', icon: MapIcon, current: false},
+    {name: 'Statistics', href: '/statistics', icon: ChartBarIcon, current: false},
 ]
 const userNavigation = [
-    { name: 'Your Profile', href: '#' },
-    { name: 'Settings', href: '#' },
-    { name: 'Sign out', href: '#' },
+    {name: 'Your Profile', href: '#'},
+    {name: 'Settings', href: '#'},
+    {name: 'Sign out', href: '#'},
 ]
 
 function classNames(...classes) {
@@ -58,6 +46,8 @@ export default function Master(props) {
 
     return (
         <div className="h-screen flex overflow-hidden bg-gray-100">
+            <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
+
             <Transition.Root show={sidebarOpen} as={Fragment}>
                 <Dialog
                     as="div"
@@ -75,7 +65,7 @@ export default function Master(props) {
                         leaveFrom="opacity-100"
                         leaveTo="opacity-0"
                     >
-                        <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75" />
+                        <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75"/>
                     </Transition.Child>
                     <Transition.Child
                         as={Fragment}
@@ -102,7 +92,7 @@ export default function Master(props) {
                                         onClick={() => setSidebarOpen(false)}
                                     >
                                         <span className="sr-only">Close sidebar</span>
-                                        <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                                        <XIcon className="h-6 w-6 text-white" aria-hidden="true"/>
                                     </button>
                                 </div>
                             </Transition.Child>
@@ -150,11 +140,7 @@ export default function Master(props) {
                     {/* Sidebar component, swap this element with another sidebar if you like */}
                     <div className="flex flex-col h-0 flex-1">
                         <div className="flex items-center h-16 flex-shrink-0 px-4 bg-gray-900">
-                            <img
-                                className="h-8 w-auto"
-                                src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                                alt="Workflow"
-                            />
+                            <h1 className="text-gray-100 text-xl font-bold">Strava Heatmap</h1>
                         </div>
                         <div className="flex-1 flex flex-col overflow-y-auto">
                             <nav className="flex-1 px-2 py-4 bg-gray-800 space-y-1">
@@ -189,24 +175,26 @@ export default function Master(props) {
                         onClick={() => setSidebarOpen(true)}
                     >
                         <span className="sr-only">Open sidebar</span>
-                        <MenuAlt2Icon className="h-6 w-6" aria-hidden="true" />
+                        <MenuAlt2Icon className="h-6 w-6" aria-hidden="true"/>
                     </button>
                     <div className="flex-1 px-4 flex justify-between">
                         <div className="flex-1 flex items-center">
                             <h1 className="text-2xl font-semibold text-gray-900">{props.title}</h1>
                         </div>
                         <div className="ml-4 flex items-center md:ml-6">
-                            <button className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            <button
+                                className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                 <span className="sr-only">View notifications</span>
-                                <BellIcon className="h-6 w-6" aria-hidden="true" />
+                                <BellIcon className="h-6 w-6" aria-hidden="true"/>
                             </button>
 
                             {/* Profile dropdown */}
                             <Menu as="div" className="ml-3 relative">
-                                {({ open }) => (
+                                {({open}) => (
                                     <>
                                         <div>
-                                            <Menu.Button className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                            <Menu.Button
+                                                className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                                 <span className="sr-only">Open user menu</span>
                                                 <img
                                                     className="h-8 w-8 rounded-full"
@@ -231,7 +219,7 @@ export default function Master(props) {
                                             >
                                                 {userNavigation.map((item) => (
                                                     <Menu.Item key={item.name}>
-                                                        {({ active }) => (
+                                                        {({active}) => (
                                                             <a
                                                                 href={item.href}
                                                                 className={classNames(
--- ui/pages/dashboard/index.js
+++ ui/pages/dashboard/index.js
@@ -1,11 +1,15 @@
 import Master from "../../layouts/master";
 import Statistics from "../../layouts/statistics";
+import Notification from "../../layouts/notification";
+import Table from "../../layouts/tabls";
 
 export default function Dashboard() {
     return (
         <Master title="Dashboard">
 
-            <Statistics/>
+            <Table/>
+
+            <Notification/>
 
         </Master>
     )
