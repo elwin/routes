6187b643a9afde63939ffc49041a83fce297fa26
Elwin Stephan <elwin.stephan@gmail.com>
2021-04-18 12:37:46 +0200

    add next.js again

---
+++ ui/pages/login/index.js
@@ -0,0 +1,40 @@
+/*
+  This example requires Tailwind CSS v2.0+
+
+  This example requires some changes to your config:
+
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ]
+  }
+  ```
+*/
+export default function Example() {
+    return (
+        <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+            </div>
+
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                    <form className="space-y-6" action="#" method="POST">
+                        <div>
+                            <button
+                                type="submit"
+                                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            >
+                                Authenticate with Strava
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ ui/pages/map/index.js
@@ -0,0 +1,12 @@
+import Master from "../../layouts/master";
+import Statistics from "../../layouts/statistics";
+
+export default function Dashboard() {
+    return (
+        <Master title="Map">
+
+        </Master>
+    )
+}
+
+
